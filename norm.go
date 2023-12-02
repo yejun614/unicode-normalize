171b54a48b67d24ca744e4f12da83cfb78ce9cce
YeJun, Jung <yejun614@naver.com>
2023-12-03 01:11:34 +0900

    Feat: add command stdin

--- norm.go
+++ norm.go
@@ -18,118 +18,122 @@
 * [Changelogs]
 * 2023-02-25
 *  - Create norm program with golang
-*/
+* 2023-12-03
+*  - Add command stdin
+ */
 
 package main
 
 import (
-  "os"
-  "io"
-  "fmt"
-  "flag"
-  "bufio"
-  "strings"
-  "os/exec"
-  "github.com/fatih/color"
-  "golang.org/x/text/unicode/norm"
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/fatih/color"
+	"golang.org/x/text/unicode/norm"
 )
 
 var (
-  DebugMode bool
+	DebugMode bool
 )
 
 func main() {
-  // flags
-  flag.Usage = help
-  flag.BoolVar(&DebugMode, "debug", false, "Turn on the debugging mode")
-  shell := flag.String("sh", "bash -c", "shell")
-  flag.Parse()
+	// flags
+	flag.Usage = help
+	flag.BoolVar(&DebugMode, "debug", false, "Turn on the debugging mode")
+	shell := flag.String("sh", "bash -c", "shell")
+	flag.Parse()
 
-  if args := flag.Args(); len(args) > 0 {
-    // run mode
-    run(*shell, args)
+	if args := flag.Args(); len(args) > 0 {
+		// run mode
+		run(*shell, args)
 
-  } else {
-    // scan mode
-    scan(os.Stdin)
-  }
+	} else {
+		// scan mode
+		scan(os.Stdin)
+	}
 }
 
 func run(shell string, args []string) {
-  shellArgs := strings.Fields(shell)
-  cmdArgs := append(shellArgs[1:], strings.Join(args, " "))
+	shellArgs := strings.Fields(shell)
+	cmdArgs := append(shellArgs[1:], strings.Join(args, " "))
 
-  cmd := exec.Command(shellArgs[0], cmdArgs...);
-  out, err := cmd.StdoutPipe()
+	cmd := exec.Command(shellArgs[0], cmdArgs...)
+	cmd.Stdin = os.Stdin
+	out, err := cmd.StdoutPipe()
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  cmd.Start()
-  scan(out)
-  cmd.Wait()
+	cmd.Start()
+	scan(out)
+	cmd.Wait()
 }
 
 func scan(r io.Reader) {
-  scanner := bufio.NewScanner(r)
+	scanner := bufio.NewScanner(r)
 
-  for {
-    scanner.Scan()
+	for {
+		scanner.Scan()
 
-    if str := scanner.Bytes(); len(str) != 0 {
-      if DebugMode {
-        buf := norm.NFC.Bytes(str)
+		if str := scanner.Bytes(); len(str) != 0 {
+			if DebugMode {
+				buf := norm.NFC.Bytes(str)
 
-        color.Set(color.FgWhite)
-        fmt.Print("[DEBUG]")
+				color.Set(color.FgWhite)
+				fmt.Print("[DEBUG]")
 
-        color.Set(color.FgCyan)
-        fmt.Print(" INPUT  <<< ")
-        for index, el := range str {
-          if index >= len(buf) || el != buf[index] {
-            color.Set(color.FgRed)
-          } else {
-            color.Set(color.FgCyan)
-          }
-          fmt.Printf("%d ", el)
-        }
-        fmt.Println("")
+				color.Set(color.FgCyan)
+				fmt.Print(" INPUT  <<< ")
+				for index, el := range str {
+					if index >= len(buf) || el != buf[index] {
+						color.Set(color.FgRed)
+					} else {
+						color.Set(color.FgCyan)
+					}
+					fmt.Printf("%d ", el)
+				}
+				fmt.Println("")
 
-        color.Set(color.FgMagenta)
-        fmt.Print("        OUTPUT >>> ")
-        for index, el := range buf {
-          if index >= len(str) || el != str[index] {
-            color.Set(color.FgRed)
-          } else {
-            color.Set(color.FgMagenta)
-          }
-          fmt.Printf("%d ", el)
-        }
-        fmt.Println("")
-      } else {
-        os.Stdout.Write(norm.NFC.Bytes(str))
-        os.Stdout.Write([]byte{ 10 }) // new line
-      }
-    } else {
-      break
-    }
-  }
+				color.Set(color.FgMagenta)
+				fmt.Print("        OUTPUT >>> ")
+				for index, el := range buf {
+					if index >= len(str) || el != str[index] {
+						color.Set(color.FgRed)
+					} else {
+						color.Set(color.FgMagenta)
+					}
+					fmt.Printf("%d ", el)
+				}
+				fmt.Println("")
+			} else {
+				os.Stdout.Write(norm.NFC.Bytes(str))
+				os.Stdout.Write([]byte{10}) // new line
+			}
+		} else {
+			break
+		}
+	}
 }
 
 func help() {
-  fmt.Fprintf(os.Stderr, "Unicode Normarlize (v.0.1)\n\n")
+	fmt.Fprintf(os.Stderr, "Unicode Normarlize (v.0.1)\n\n")
 
-  fmt.Fprintf(os.Stderr, "[Usage]\n")
-  flag.PrintDefaults()
+	fmt.Fprintf(os.Stderr, "[Usage]\n")
+	flag.PrintDefaults()
 
-  fmt.Fprintf(os.Stderr, "\n[Example]\n")
-  fmt.Fprintf(os.Stderr, " > norm ls ~                    # bash -c \"ls ~\"\n")
-  fmt.Fprintf(os.Stderr, " > norm -sh \"fish -c\" ls ~      # fish -c \"ls ~\"\n")
-  fmt.Fprintf(os.Stderr, "\n")
-  fmt.Fprintf(os.Stderr, " > alias ls=\"norm ls\"\n")
-  fmt.Fprintf(os.Stderr, " > ls ~                         # norm ls ~\n")
-  fmt.Fprintf(os.Stderr, " > ls -al ~                     # norm ls -al ~\n")
-  fmt.Fprintf(os.Stderr, "\n")
-  fmt.Fprintf(os.Stderr, " > ls ~ | norm\n")
+	fmt.Fprintf(os.Stderr, "\n[Example]\n")
+	fmt.Fprintf(os.Stderr, " > norm ls ~                    # bash -c \"ls ~\"\n")
+	fmt.Fprintf(os.Stderr, " > norm -sh \"fish -c\" ls ~      # fish -c \"ls ~\"\n")
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, " > alias ls=\"norm ls\"\n")
+	fmt.Fprintf(os.Stderr, " > ls ~                         # norm ls ~\n")
+	fmt.Fprintf(os.Stderr, " > ls -al ~                     # norm ls -al ~\n")
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, " > ls ~ | norm\n")
 }
